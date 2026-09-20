efcea2ded44d5bbbd0e9ccb48536745d35e0be4a
Lucas-Sanches <lucas.smbm.2008@gmail.com>
2026-09-20 05:00:37 -0300

    Update File

--- source/import.hx
+++ source/import.hx
@@ -1,17 +1,14 @@
-#if !macro
 #if DISCORD_ALLOWED
-// New Discord API
 import funkin.utils.api.DiscordAPI;
 #end
 
 #if LUA_ALLOWED
-// Psych
 import llua.*;
 import llua.Lua;
 #end
 
 #if ACHIEVEMENTS_ALLOWED
-import funkin.states.achievements.config.Achievements;
+import funkin.states.achievements.data.Achievements;
 #end
 
 #if sys
@@ -21,41 +18,47 @@ import sys.io.*;
 import js.html.*;
 #end
 
-// New Souce Code folders (Pico Engine v2.7.26)
+// Pico Engine Stuff - v2.26.7
 import funkin.Paths;
 import funkin.data.ClientPrefs;
+import funkin.data.objects.Alphabet;
+
 import funkin.states.PlayState;
+import funkin.states.LoadingScreenMenuState;
+import funkin.states.MusicBeatState;
+import funkin.states.TitleMenuState;
+import funkin.states.CreditsMenuState;
+import funkin.states.menus.MainMenuState;
+import funkin.states.menus.StoryModeMenuState;
+import funkin.states.options.OptionsMenuState;
+
+import funkin.play.Rank;
 import funkin.play.Difficulty;
 import funkin.play.Conductor;
+
 import funkin.stages.BaseStage;
 import funkin.stages.BGSprite;
-import funkin.states.LoadingScreenState;
-import funkin.states.MusicBeatState;
+
 import funkin.utils.Controls;
 import funkin.utils.CoolUtil;
-import funkin.utils.Alphabet;
-import funkin.menus.MainMenuState;
-import funkin.translations.Language;
 import funkin.utils.CustomFadeTransition;
+import funkin.utils.engines.pico.EnginePerformance;
+
+import funkin.translations.Language;
 import funkin.substates.MusicBeatSubstate;
+import funkin.modding.ModsMenuState;
 
 #if MODS_ALLOWED
 import funkin.modding.Mods;
 #end
 
 #if LUA_ALLOWED
-import funkin.modding.scripting.FunkinLua;
+import funkin.modding.scripting.FunkinLuaProgramming;
 import funkin.modding.scripting.psychlua.*;
 #end
 
-import funkin.states.TitleState;
-import funkin.states.CreditsState;
-import funkin.menus.StoryMenuState;
-import funkin.modding.ModsMenuState;
-import funkin.states.options.OptionsState;
-
 #if PSYCH_ALLOWED
-// Psych UI'S elements
+// Psych UI Elements
 import funkin.utils.engines.psych.ui.*;
 #end
 
@@ -90,7 +93,25 @@ import flixel.util.FlxStringUtil;
 import flixel.util.FlxSave;
 import flixel.input.keyboard.FlxKey;
 import flixel.animation.FlxAnimationController;
+import flixel.FlxSubState;
+import flixel.util.FlxSave;
+import flixel.util.FlxSort;
+import flixel.util.FlxSpriteUtil;
+import flixel.util.FlxStringUtil;
+import flixel.util.FlxDestroyUtil;
+import flixel.input.keyboard.FlxKey;
+import flixel.graphics.frames.FlxAtlasFrames;
+
+import lime.utils.Assets;
+import lime.media.AudioBuffer;
+
+import flash.media.Sound;
+import flash.geom.Rectangle;
+
+import haxe.Json;
+import haxe.Exception;
+import haxe.io.Bytes;
+
 import openfl.media.Sound;
 import lime.utils.Assets;
 using StringTools;
-#end
